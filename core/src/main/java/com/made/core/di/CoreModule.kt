4c1e8543998fea11b7f264c862e3b61307b03a65
fatwaabdusyukur <waengyoo@gmail.com>
2022-02-27 21:40:21 +0700

    Add SQLChipper, Obfuscate, and SSL Pinning

--- core/src/main/java/com/made/core/di/CoreModule.kt
+++ core/src/main/java/com/made/core/di/CoreModule.kt
@@ -8,26 +8,49 @@ import com.made.core.data.source.local.room.GDocsDatabase
 import com.made.core.data.source.remote.RemoteDataSource
 import com.made.core.data.source.remote.network.ApiServices
 import com.made.core.domain.repository.IGDocsRepository
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 val dataBaseModule = module {
     factory { get<GDocsDatabase>().getGDocsDao() }
     single {
+        val encryptKey : ByteArray = SQLiteDatabase.getBytes(BuildConfig.PASSPHRASE.toCharArray())
+        val factory = SupportFactory(encryptKey)
         Room.databaseBuilder(
             androidContext(),
-            GDocsDatabase::class.java, "GDocs.db"
-        ).fallbackToDestructiveMigration().build()
+            GDocsDatabase::class.java, BuildConfig.DATABASE
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 }
 
 val networkModule = module {
     single {
+
+        val certificatePinner = CertificatePinner.Builder()
+            .add(BuildConfig.HOSTNAME,
+                BuildConfig.PIN_A,
+                BuildConfig.PIN_B
+                ).build()
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
+            .build()
+
         val retrofit = Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
             .build()
         retrofit.create(ApiServices::class.java)
     }
